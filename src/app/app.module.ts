28acc9cc1a0966030f55e3fc3c72e73c7798271a
hasanmertmercan <mercan.hasanmert@gmail.com>
2019-10-17 00:47:10 +0200

    Style - added MainPage modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ProductModule } from './products/product.module';
 
 
 @NgModule({
@@ -19,8 +19,8 @@ import { MainPageComponent } from './main-page/main-page.component';
       { path: '', redirectTo: 'anasayfa', pathMatch: 'full' },
       { path: '**', redirectTo: 'anasayfa', pathMatch: 'full' }
     ]),
+    //ProductModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { MainPageComponent } from '../main-page/main-page.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MainPageComponent,
+    RouterModule.forChild([
+      { path: 'product', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent},
+      // { path: '', redirectTo: 'anasayfa', pathMatch: 'full' },
+      // { path: '**', redirectTo: 'anasayfa', pathMatch: 'full' }
+    ]),
   ],
   declarations: [
   ProductListComponent,
